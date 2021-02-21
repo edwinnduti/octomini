bd5d48cb819382724e10d75ee7003c9c8acb2461
edwinnduti <nduti316@gmail.com>
2021-02-21 08:09:44 +0300

    made it in single code

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/urfave/negroni v1.0.0
+	go.mongodb.org/mongo-driver v1.4.6
 )
--- main.go
+++ main.go
@@ -10,12 +10,98 @@ package main
 
 // libraries to use
 import (
-	"os"
+	"github.com/urfave/negroni"
+	"context"
+	"encoding/json"
+	"html/template"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
-	"github.com/urfave/negroni"
-	"github.com/edwinnduti/octomini/router"
+	"os"
+	"time"
 )
+
+// templ
+var (
+	dir = "assets/"
+	templ = template.Must(template.ParseGlob("templates/*.html"))
+)
+
+// Member type
+type Member struct{
+	Name		string	`json:"name"`
+	Offering	string	`json:"offering"`
+}
+
+// database and collection names are statically declared
+const database, collection = "pceanyaga", "offeringManagement"
+
+// create connection to mongodb
+func CreateConnection() (*mongo.Client, error) {
+	// connect to mongodb
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// set MONGOURI
+	MongoURI := os.Getenv("MONGOURI")
+	// connect to mongodb
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
+		MongoURI,
+	))
+	Check(err)
+
+	// return client and error
+	return client, nil
+}
+
+func Home(w http.ResponseWriter,r *http.Request){
+	//render template
+	err := templ.ExecuteTemplate(w,"index.html",nil)
+	Check(err)
+}
+
+/* GET all  data */
+func GetAllHandler(w http.ResponseWriter,r *http.Request){
+        var members []Member
+
+        // create connection
+        client, err := CreateConnection()
+	Check(err)
+
+	// Decode the incoming Data json
+	err = json.NewDecoder(r.Body).Decode(&members)
+        Check(err)
+
+	// select db and collection
+	cHotel := client.Database(database).Collection(collection)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// get all documents
+	cursor,err := cHotel.Find(ctx, bson.M{})
+	Check(err)
+
+	err = cursor.All(ctx,&members)
+	Check(err)
+
+	// set headers
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Method", "GET")
+	w.WriteHeader(http.StatusOK)
+
+	//render template
+	err = templ.ExecuteTemplate(w,"index.html",members)
+}
+
+/* log errors */
+func Check(err error){
+	if err != nil{
+		log.Fatalln(err)
+	}
+}
 
 
 // Main function
@@ -29,7 +115,11 @@ func main() {
 	*/
 
 	//Register router{}
-	r := router.Router()
+	r := mux.NewRouter().StrictSlash(false)
+
+	// API routes,handlers and methods
+	r.HandleFunc("/",GetAllHandler).Methods("GET","OPTIONS")
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir(dir))))
 
 	//Get port
 	Port := os.Getenv("PORT")
