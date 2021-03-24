b3af86c5f78a1d8dbb4929b8d15fb65df4fdec14
edwinnduti <nduti316@gmail.com>
2021-03-24 17:44:40 +0300

    added POST, GET, OPTIONS, PUT, DELETE

--- main.go
+++ main.go
@@ -252,7 +252,7 @@ func UpdateProfile(w http.ResponseWriter,r *http.Request){
 
 	// set headers
 	w.Header().Set("Access-Control-Allow-Origin","*")
-	w.Header().Set("Access-Control-Allow-Method","PUT")
+	w.Header().Set("Access-Control-Allow-Methods","POST, GET, OPTIONS, PUT, DELETE")
 	//w.WriteHeader(http.StatusOK)
 
 	//redirect to profile
